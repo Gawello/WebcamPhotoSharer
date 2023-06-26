f646ecdc3e5b9ba83efa614324c44d1a441edf8a
Gaweł Banasik <83306952+Gawello@users.noreply.github.com>
2023-06-26 22:06:56 +0200

    Camera button improve and capture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time
 
 from filesharer import FileSharer
 
@@ -11,12 +12,17 @@ class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
         self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
 
 
 class ImageScreen(Screen):
