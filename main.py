5331ddd2db8e2c9646bda501beaf0e8616c8019f
Gawello <83306952+Gawello@users.noreply.github.com>
2023-06-28 11:40:11 +0200

    HOW TO FIX CAMERA!?

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
+        # Screwed part... Need to read docs
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
